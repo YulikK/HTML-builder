7bcff02666bbe1a34144472798a0cd21a0757e72
YulikK <yulikpoch@gmail.com>
2024-01-15 18:41:17 +0100

    feat: add task with file information(03)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const folderPath = path.join(__dirname, "secret-folder");
+
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+  for (const file of files) {
+
+    const filePath = path.join(file.path, file.name);
+
+    fs.stat(filePath, (error, stats) => { 
+
+      if(error) throw console.error(error);
+      
+      const fileSize = stats.size / 1000; 
+      const fileExtension = file.name.split('.').slice(1);
+      console.log(`${file.name} - ${fileExtension} - ${fileSize}kb`);
+      
+    }); 
+  }
+});
