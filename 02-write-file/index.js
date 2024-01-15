42e4af6ebd5f40ae48d19a07e3950af56966edf6
YulikK <yulikpoch@gmail.com>
2024-01-15 16:04:25 +0100

    feat: add write file task(02)

---
+++ 02-write-file/index.js
@@ -0,0 +1,21 @@
+const { stdin, stdout } = process;
+const fs = require("fs");
+const path = require("path");
+
+const output = fs.createWriteStream(path.join(__dirname, "write-text.txt"));
+
+stdout.write("What are we gonna put in the file?\n");
+stdout.write("new text line: ");
+
+stdin.on("data", (data) => {
+
+  if (data.toString().split('\n').join('') === 'exit') {
+    process.exit();
+  } else {
+    output.write(data);
+    stdout.write("new text line: ");
+  }
+
+});
+
+process.on("exit", () => stdout.write("Goodbye!"));
