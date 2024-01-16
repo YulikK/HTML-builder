a11c0f3c40fcdc601c493b5769c5023d45bfc32a
YulikK <yulikpoch@gmail.com>
2024-01-16 16:26:31 +0100

    feat: add bundler task(05)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,20 @@
+// const { mkdir, readdir, stat, copyFile, rmdir, unlink } = require('node:fs/promises');
+const fs = require("fs").promises;
+const path = require('path');
+
+async function createBundle(folderPath) {
+  const files = await fs.readdir(folderPath);
+  const cssFiles = files.filter(file => file.endsWith('.css'));
+  let cssContent = '';
+
+  for (const file of cssFiles) {
+    const filePath = path.join(folderPath, file);
+    const fileContent = await fs.readFile(filePath, 'utf-8');
+    cssContent += fileContent;
+  }
+
+  const outputPath = path.join(__dirname, 'project-dist', 'bundle.css');
+  await fs.writeFile(outputPath, cssContent);
+}
+
+createBundle(path.join(__dirname, 'styles'));
