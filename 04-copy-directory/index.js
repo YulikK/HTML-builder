2747effc2432f5374401cd4999fb84014eea6fe3
YulikK <yulikpoch@gmail.com>
2024-01-18 11:41:43 +0100

    refactor: task 04,  make a structure initialize function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,58 +1,70 @@
-const {
-  mkdir,
-  readdir,
-  stat,
-  copyFile,
-  rmdir,
-  unlink,
-} = require('node:fs/promises');
+const fs = require('fs').promises;
 const path = require('path');
 
-const startFolder = 'files';
-const newFolder = 'files-copy';
-const taskFolder = path.dirname(__filename);
+const folder = 'files';
+const copyFolder = 'files-copy';
+const pathTask = path.dirname(__filename);
+const pathFolder = path.join(pathTask, folder);
+const pathCopyFolder = path.join(pathTask, copyFolder);
 
-async function dirExists(folderPath) {
-  try {
-    await stat(folderPath);
-    return true;
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      return false;
-    }
-  }
+function startTask() {
+  return fs
+    .access(pathCopyFolder)
+    .then(() => {
+      console.log(`1. Looking for the ${copyFolder} folder`);
+      return clearDirectory(pathCopyFolder);
+    })
+    .catch(() => {
+      console.log(`1. Create directory ${copyFolder}`);
+      return fs.mkdir(pathCopyFolder);
+    })
+    .then(() => {
+      console.log(`2. Copying from ${folder} folder`);
+      return copyDirectory(pathFolder, pathCopyFolder);
+    });
 }
 
-async function copyFiles() {
-  try {
-    const isExist = dirExists(path.join(taskFolder, newFolder));
-    if (isExist) {
-      const filesOld = await readdir(path.join(taskFolder, newFolder));
-      for (const file of filesOld) {
-        const stats = await stat(path.join(taskFolder, newFolder, file));
-        if (stats.isFile()) {
-          await unlink(path.join(taskFolder, newFolder, file));
+function clearDirectory(directory) {
+  return fs.readdir(directory).then((files) => {
+    const unlinkPromises = files.map((file) => {
+      const filePath = path.join(directory, file);
+      return fs
+        .stat(filePath)
+        .then((stat) => {
+          if (stat.isDirectory()) {
+            return clearDirectory(filePath).then(() => fs.rmdir(filePath));
+          } else {
+            return fs.unlink(filePath);
+          }
+        })
+        .catch((err) => {
+          console.error(`Error deleting a file ${filePath}:`, err);
+        });
+    });
+
+    return Promise.all(unlinkPromises);
+  });
+}
+
+function copyDirectory(source, destination) {
+  return fs.readdir(source).then((files) => {
+    const copyPromises = files.map((file) => {
+      const sourcePath = path.join(source, file);
+      const destPath = path.join(destination, file);
+
+      return fs.stat(sourcePath).then((stat) => {
+        if (stat.isDirectory()) {
+          return fs
+            .mkdir(destPath)
+            .then(() => copyDirectory(sourcePath, destPath));
+        } else {
+          return fs.copyFile(sourcePath, destPath);
         }
-      }
-      await rmdir(path.join(taskFolder, newFolder));
-    }
+      });
+    });
 
-    await mkdir(path.join(taskFolder, newFolder), { recursive: true });
-
-    const files = await readdir(path.join(taskFolder, startFolder));
-
-    for (const file of files) {
-      const stats = await stat(path.join(taskFolder, startFolder, file));
-      if (stats.isFile()) {
-        await copyFile(
-          path.join(taskFolder, startFolder, file),
-          path.join(taskFolder, newFolder, file),
-        );
-      }
-    }
-  } catch (error) {
-    console.error('Error copying files:', error);
-  }
+    return copyPromises;
+  });
 }
 
-copyFiles();
+startTask();
