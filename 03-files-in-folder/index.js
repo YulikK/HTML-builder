7d87dbc4a272d2f89538e345df85e6ab49ed793e
YulikK <yulikpoch@gmail.com>
2024-01-20 10:29:40 +0100

    feat: add isFile checking

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,15 +5,17 @@ const folderPath = path.join(__dirname, 'secret-folder');
 function startTask() {
   return fs.readdir(folderPath, { withFileTypes: true }).then((files) => {
     files.map((file) => {
-      const filePath = path.join(file.path, file.name);
-      return fs.stat(filePath).then((stats) => {
-        const fileInfo = {
-          name: file.name.split('.').slice(0, 1),
-          type: file.name.split('.').slice(1),
-          size: stats.size,
-        };
-        console.log(`${fileInfo.name} - ${fileInfo.type} - ${fileInfo.size} B`);
-      });
+      if(file.isFile()) {
+        const filePath = path.join(file.path, file.name);
+        return fs.stat(filePath).then((stats) => {
+          const fileInfo = {
+            name: file.name.split('.').slice(0, 1),
+            type: file.name.split('.').slice(1),
+            size: stats.size,
+          };
+          console.log(`${fileInfo.name} - ${fileInfo.type} - ${fileInfo.size} B`);
+        });
+      }
     });
   });
 }
